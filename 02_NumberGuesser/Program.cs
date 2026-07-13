e34356842a4b581413cddef3da6eec8b74642faa
TudorM-Dev <tudormutica21@gmail.com>
2026-07-13 18:20:59 +0300

    02_NumberGuesser

---
+++ 02_NumberGuesser/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _02_NumberGuesser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            bool playAgain = true;
+            int min = 1, max = 100;
+            int guess, number, guesses;
+
+            while(playAgain)
+            {
+                guess = 0;
+                guesses = 0;
+                number = random.Next(1, max + 1);
+
+
+                while(guess != number)
+                {
+                    Console.WriteLine($"Guess a number between {min} - {max}: ");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    
+                    if(guess > number) Console.WriteLine(guess + " is to high");
+                    else if (guess < number) Console.WriteLine(guess + " is to low");
+                    guesses++;
+                }
+                Console.WriteLine("YOU WIN :P");
+                Console.WriteLine("You needed " + guesses + " guesses");
+
+                Console.WriteLine("Wanna play again? (Y=yes, N=no)");
+                if (Console.ReadLine().ToUpper() == "N") playAgain = false;
+            }
+
+            Console.WriteLine("Bye bye :P");
+
+        }
+    }
+}
